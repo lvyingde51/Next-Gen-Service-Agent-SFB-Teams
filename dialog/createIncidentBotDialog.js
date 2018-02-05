b5139fd8a46af394a584a91811c66465c73d195f
Arun Prasaath <39402.hexaware@gmail.com>
2018-02-05 19:13:23 +0530

    AP : API Service Create Incident changing text cases

--- dialog/createIncidentBotDialog.js
+++ dialog/createIncidentBotDialog.js
@@ -9,7 +9,7 @@
 
     module.exports.beginDialog= [
         function (session) {
-            builder.Prompts.choice(session, 'What is the Severity?', ['High', 'Medium', 'Low']);
+            builder.Prompts.choice(session, 'What is the severity?', ['High', 'Medium', 'Low']);
         },
         function(session, results) {
             session.userData.severity = results.response.entity;
@@ -23,7 +23,7 @@
     ];
     module.exports.shortDescription= [
         function (session) {
-            builder.Prompts.text(session, 'I need your (Short) Description of the incident');
+            builder.Prompts.text(session, 'I need your (short) description of the incident');
         },
         function(session, results) {
             session.userData.shortDescription = results.response;
@@ -37,7 +37,7 @@
     ];
     module.exports.category= [
         function (session) {
-            builder.Prompts.choice(session, 'Choose any one Category of the incident from the below list', ['Inquiry/Help','Software','Hardware','Network','Database']);
+            builder.Prompts.choice(session, 'Choose any one category of the incident from the below list', ['Inquiry/Help','Software','Hardware','Network','Database']);
         },
         function(session, results) {
             session.userData.category = results.response.entity;
@@ -59,7 +59,7 @@
             apiService.createIncidentService(JSON.parse(JSON.stringify(objData)), function (data) {
                 console.log('Incident No : ',data.result.number);
                 console.log('Total Response : ',JSON.stringify(data));
-                let msg = 'Successfully created incident:- \nIncident Id : '+data.result.number+'\nUrgency : '+objData.urgency+'\Category : '+objData.category+'\nShort Description : '+objData.short_description+' \nStatus: New \n Your Incident will be assigned to a live agent shortly and your incident will be followed from there. \n Or You can check status of your incident by typing your incident ID eg: INC1234567';
+                let msg = 'Successfully created incident:- \nIncident Id : '+data.result.number+'\nUrgency : '+objData.urgency+'\nCategory : '+objData.category+'\nShort Description : '+objData.short_description+' \nStatus: New \n Your incident will be assigned to a live agent shortly and your incident will be followed from there (or) you can check status of your incident by typing your incident number eg: INC1234567';
                 session.endDialog(msg);
             });            
         }
