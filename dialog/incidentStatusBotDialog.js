0fae7f6eeac9bf5c9374a7af479ba667900091ec
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-02-05 09:41:25 +0530

    MM : Added new file for making API calls and implemented getting the status of incident for an incident number

--- dialog/incidentStatusBotDialog.js
+++ dialog/incidentStatusBotDialog.js
@@ -1,8 +1,9 @@
 (function () {
     'use strict';
-    
-    var builder = require('botbuilder');    
+
+    var builder = require('botbuilder');
     var log = require('../utils/logs');
+    var apiService = require('../server/apiServices');
     var incidentArr = [];
 
     // Incident Request Status List
@@ -10,19 +11,19 @@
         function (session) {
             builder.Prompts.choice(session, 'How do you want me to search it?', ['By Incident Id', 'Last 10 Incidents']);
         },
-        function(session, results) {
+        function (session, results) {
             session.userData.ISSearchType = results.response.entity;
 
-            if(session.userData.ISSearchType === 'By Incident Id') {
-                session.beginDialog('isSearchById', function(err) {
-                    if(err) {
+            if (session.userData.ISSearchType === 'By Incident Id') {
+                session.beginDialog('isSearchById', function (err) {
+                    if (err) {
                         session.send(new builder.Message().text('Error Occurred with isSearchById' + err.message));
                     }
                 });
             }
-            if(session.userData.ISSearchType === 'Last 10 Incidents') {
-                session.beginDialog('isSearchByList', function(err) {
-                    if(err) {
+            if (session.userData.ISSearchType === 'Last 10 Incidents') {
+                session.beginDialog('isSearchByList', function (err) {
+                    if (err) {
                         session.send(new builder.Message().text('Error Occurred with isSearchByList' + err.message));
                     }
                 });
@@ -35,11 +36,14 @@
         function (session) {
             builder.Prompts.text(session, 'Please provide your Incident Id');
         },
-        function(session, results) {
+        function (session, results) {
             session.userData.ISIncidentId = results.response;
-        // Make API call to Service Now with Incident Id and get Response...
-            let msg = 'Below are the details for the requested incident:- \nIncident Id : INC 0010410 \nShort Description : Mouse not working \nStatus: In Progress \nAssigned To: Don Goodliffe \nWhat do you want to do next?';
-            session.endDialog(msg);
+            // Make API call to Service Now with Incident Id and get Response...
+            apiService.getIncidentStatusByNumber(session.userData.ISIncidentId, function (data) {
+                log.consoleDefault(JSON.stringify(data));
+                let msg = 'Below are the details for the requested incident:- \nIncident Id : ' + session.userData.ISIncidentId + ' \nShort Description : '+ data.result[0].short_description +' \nStatus: In Progress \nAssigned To: '+ data.result[0].assigned_to +' \nWhat do you want to do next?';
+                session.endDialog(msg);
+            });
         }
     ];
 
@@ -50,7 +54,7 @@
             incidentArr = [];
             builder.Prompts.choice(session, 'List of Incidents', ['INC 0010410', 'INC 0010411', 'INC 0010412', 'INC 0010413', 'INC 0010414']);
         },
-        function(session, results) {
+        function (session, results) {
             session.userData.ISIncidentId = results.response.entity;
 
             //Filter out JSON from previous API call and display the status of Incident from **incidentArr**
---
+++ server/apiServices.js
@@ -0,0 +1,80 @@
+(function () {
+    'use strict';
+    const requestAPI = require('request');
+    var log = require('../utils/logs');
+    var builder = require('botbuilder');
+    const ServiceNowUserName = '33238';
+    const ServiceNowPwd = 'abc123';
+
+    const header = {
+        'Cache-Control': 'no-cache',
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    };
+
+    function getIncidentStatusByNumber(ticketnumber, callback) {
+        try {
+            var options = {
+                url: 'https://dev18442.service-now.com/api/now/v1/table/incident',
+                qs: { number: ticketnumber },
+                method: 'GET',
+                header: header,
+                body: '',
+                json: true,
+                auth: {
+                    user: ServiceNowUserName,
+                    password: ServiceNowPwd
+                }
+            };
+
+            requestAPI(options, function (error, response, body) {
+                if (error) {
+                    log.consoleDefault(JSON.stringify(error));
+                    return
+                  }
+                  else {
+                    log.consoleDefault('headers:' + response.headers);
+                    log.consoleDefault('status code:' + response.statusCode);
+                    callback(body);
+                  }
+            });
+        }
+        catch(err) {
+            log.consoleDefault(JSON.stringify(err));
+        }        
+    };
+
+    function getIncidentStatusByList(callback) {
+        try {
+            var options = {
+                url: 'https://dev18442.service-now.com/api/now/v1/table/incident',
+                method: 'GET',
+                header: header,
+                body: '',
+                json: true,
+                auth: {
+                    user: ServiceNowUserName,
+                    password: ServiceNowPwd
+                }
+            };
+
+            requestAPI(options, function (error, response, body) {
+                if (error) {
+                    log.consoleDefault(JSON.stringify(error));
+                    return
+                  }
+                  else {
+                    log.consoleDefault('headers:' + response.headers);
+                    log.consoleDefault('status code:' + response.statusCode);
+                    callback(null, body);
+                  }
+            });
+        }
+        catch(err) {
+            log.consoleDefault(JSON.stringify(err));
+        }        
+    };
+
+    module.exports.getIncidentStatusByNumber = getIncidentStatusByNumber;
+    module.exports.getIncidentStatusByList = getIncidentStatusByList;
+}());
