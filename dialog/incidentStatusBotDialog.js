25ac61de9f9138de42185b061d064153cf9452e8
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-02-06 15:20:40 +0530

    MM : Made changes for getting responses for Assigned to when no value is obtained from API

--- dialog/incidentStatusBotDialog.js
+++ dialog/incidentStatusBotDialog.js
@@ -60,7 +60,7 @@
                     let assignedTo = data.result[0].assigned_to == '' ? '-' : data.result[0].assigned_to.link;
                     log.consoleDefault(assignedTo);
                     if(assignedTo == '-') {
-                        let msg = 'Below are the details for the requested incident :- <br/>Incident Id : ' + session.conversationData.ISIncidentId + ' <br/>Short Description : ' + data.result[0].short_description + ' <br/>Status: ' + commonTemplate.incidentStatus[data.result[0].state][lang] + ' <br/>Assigned To: ' + data.result[0].assigned_to;
+                        let msg = 'Below are the details for the requested incident :- <br/>Incident Id : ' + session.conversationData.ISIncidentId + ' <br/>Short Description : ' + data.result[0].short_description + ' <br/>Status: ' + commonTemplate.incidentStatus[data.result[0].state][lang] + ' <br/>Assigned To: Unassigned';
                         session.endDialog(msg);
                     } else {
                         apiService.getAssignedToDetails(assignedTo, function (resp) {
