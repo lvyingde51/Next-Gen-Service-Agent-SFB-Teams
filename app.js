67ace500463fc32993c081a97f4dc49b44c4081e
Ramesh Nagineni <39586@corp.hexaware.com>
2018-02-08 19:21:30 +0530

    RN-send typing

--- app.js
+++ app.js
@@ -49,7 +49,12 @@ var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector,function (session) {
+    session.sendTyping();
+    setTimeout(function () {
+        session.send("Hello there...");
+    }, 3000);
+});
 bot.set('storage', tableStorage);
 
 const logUserConversation = (event) => {
