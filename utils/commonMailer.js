b19e25cbddd4b06048f96a3e6f5aa19ba43abf23
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-02-08 17:08:39 +0530

    MM : Added service request logging

--- utils/commonMailer.js
+++ utils/commonMailer.js
@@ -61,6 +61,42 @@
                 break;
             case "Create Service Request":
                 mailSubject = 'RE : New Service Request Logged';
+                messageContent = `<html>
+                                        <head>
+                                            <style>
+                                                p.green {
+                                                    color: green;
+                                                }
+                                                .render table, .render td, .render th {
+                                                    border: 1px solid #000;
+                                                }
+                                                .render table {
+                                                    border-collapse: collapse;
+                                                }
+                                            </style>
+                                        </head>
+                                        <body>
+                                            Hi
+                                            <br/><br/><br/>
+                                            <div class="render">
+                                                <table>
+                                                    <tr>
+                                                        <th>Service Request Item Number</th>
+                                                        <th>Urgency</th>
+                                                        <th>Requested For</th>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>` + data.sr_ID + `</td>
+                                                        <td>` + data.short_description + `</td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                            <br/><br/><br/>
+                                            <p>Please do not reply to this mail. This is an auto generated mail</p>
+                                            <br/>
+                                            <p class="green">Please do not print this email unless it is absolutely necessary.</p>
+                                        </body>
+                                    </html>`
                 break;
         }
 
