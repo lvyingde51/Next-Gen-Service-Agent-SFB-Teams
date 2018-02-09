fd680d3b39bd9a0d942d19987e703a377ac541ae
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-02-09 12:19:35 +0530

    MM : Fixing issue with welcome greeting

--- dialog/welcomeBotDialog.js
+++ dialog/welcomeBotDialog.js
@@ -8,14 +8,19 @@
 
     // Incident Request Status List
     module.exports.beginGreeting = [
-        function (session, args) {
-            var txt = `Hi ${session.message.user.name ? session.message.user.name : ' '}, I am ${process.env.AgentName}.<br/>I am here to help you out <br/>You can ask me questions like:<br/>- Create high severity incident <br/>- Incident status for 'Incident Number eg:INC0010505' <br/>- Show latest incidents <br/>- Say 'help' for any queries <br/>- Say 'goodbye' to leave conversation`;
-            var reply = new builder.Message()
-                .address(session.message.address)
-                .text(txt);
-            session.send(reply);
-            let msg = new builder.Message(session).addAttachment(createWelcomeHeroCard(session));
-            session.send(msg);
+        function (session, args, next) {
+            if (session.message.text.toUpperCase() == 'INCIDENT MANAGEMENT' || session.message.text.toUpperCase() == 'SERVICE MANAGEMENT') {
+                session.conversationData.GreetingType = session.message.text.toUpperCase();
+                next({ response: session.conversationData.GreetingType });
+            } else {
+                var txt = `Hi ${session.message.user.name ? session.message.user.name : ' '}, I am ${process.env.AgentName}.<br/>I am here to help you out <br/>You can ask me questions like:<br/>- Create high severity incident <br/>- Incident status for 'Incident Number eg:INC0010505' <br/>- Show latest incidents <br/>- Say 'help' for any queries <br/>- Say 'goodbye' to leave conversation`;
+                var reply = new builder.Message()
+                    .address(session.message.address)
+                    .text(txt);
+                session.send(reply);
+                let msg = new builder.Message(session).addAttachment(createWelcomeHeroCard(session));
+                session.send(msg);
+            }
         },
         function (session, results) {
             log.consoleDefault('Inside next fun');
@@ -32,11 +37,11 @@
     module.exports.chooseManagement = [
         function (session, args) {
             log.consoleDefault(session.message.text);
-            if (results.response.text.toUpperCase() === 'INCIDENT MANAGEMENT') {
+            if (session.conversationData.GreetingType === 'INCIDENT MANAGEMENT') {
                 let msg = new builder.Message(session).addAttachment(createIncidentHeroCard(session));
                 session.endDialog(msg);
             }
-            if (results.response.text.toUpperCase() === 'SERVICE MANAGEMENT') {
+            if (session.conversationData.GreetingType === 'SERVICE MANAGEMENT') {
                 let msg = new builder.Message(session).addAttachment(createServiceHeroCard(session));
                 session.endDialog(msg);
             }
