22f3e4ced1868db2c340d4a4e88fd3e85185c025
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-02-12 09:31:19 +0530

    MM : Added log to identify the channel source

--- dialog/welcomeBotDialog.js
+++ dialog/welcomeBotDialog.js
@@ -9,7 +9,7 @@
     // Incident Request Status List
     module.exports.beginGreeting = [
         function (session, args, next) {
-            log.consoleDefault('-----Session source----', session.message.source);
+            log.consoleDefault(session.message.source);
             if (session.message.text.toUpperCase() == 'INCIDENT MANAGEMENT' || session.message.text.toUpperCase() == 'SERVICE MANAGEMENT') {
                 session.conversationData.GreetingType = session.message.text.toUpperCase();
                 next({ response: session.conversationData.GreetingType });
