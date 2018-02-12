3b1516974282cf36b023ba22f923329620d26a26
Mubashshir Mohammed <39416@corp.hexaware.com>
2018-02-12 10:44:33 +0530

    MM : Added channel based responses for greeting seperately for skype for business and created seperate file for common messages

--- utils/botDialogs.js
+++ utils/botDialogs.js
@@ -2,81 +2,81 @@
     'use strict';
     var builder = require('botbuilder');
 
-    exports.getMessage = function (session, propertyName, language) {
-        if (!language) {
-            language = "ENGLISH";
-        }
+    // exports.getMessage = function (session, propertyName, language) {
+    //     if (!language) {
+    //         language = "ENGLISH";
+    //     }
 
-        if (session.message.user.aadObjectId) {
+    //     if (session.message.user.aadObjectId) {
 
-            let botResp = BOT_MESSAGES_TEAMS[propertyName];
+    //         let botResp = BOT_MESSAGES_TEAMS[propertyName];
 
-            if (botResp) {
-                let botMsg = botResp[language];
+    //         if (botResp) {
+    //             let botMsg = botResp[language];
 
-                if (botMsg) {
+    //             if (botMsg) {
 
-                    if (!botMsg.title) {
-                        botMsg.title = "";
-                    }
-                    if (!botMsg.subtitle) {
-                        botMsg.subtitle = "";
-                    }
-                    if (!botMsg.text) {
-                        botMsg.text = "";
-                    }
-                    if (!botMsg.buttons) {
-                        botMsg.buttons = [];
-                    }
+    //                 if (!botMsg.title) {
+    //                     botMsg.title = "";
+    //                 }
+    //                 if (!botMsg.subtitle) {
+    //                     botMsg.subtitle = "";
+    //                 }
+    //                 if (!botMsg.text) {
+    //                     botMsg.text = "";
+    //                 }
+    //                 if (!botMsg.buttons) {
+    //                     botMsg.buttons = [];
+    //                 }
 
-                    return new builder.Message(session).addAttachment(new builder.ThumbnailCard(session)
-                        .title(botMsg.title)
-                        .subtitle(botMsg.subtitle)
-                        .text(botMsg.text)
-                        .images()
-                        .buttons(botMsg.buttons));
-                } else {
-                    return 'Error :: Bot Message is not available for Property ' + propertyName + ' on Language ' + language;
-                }
+    //                 return new builder.Message(session).addAttachment(new builder.ThumbnailCard(session)
+    //                     .title(botMsg.title)
+    //                     .subtitle(botMsg.subtitle)
+    //                     .text(botMsg.text)
+    //                     .images()
+    //                     .buttons(botMsg.buttons));
+    //             } else {
+    //                 return 'Error :: Bot Message is not available for Property ' + propertyName + ' on Language ' + language;
+    //             }
 
-            } else {
-                return 'Error :: Bot Message configuration is not available for Property ' + propertyName;
-            }
+    //         } else {
+    //             return 'Error :: Bot Message configuration is not available for Property ' + propertyName;
+    //         }
 
-        } else {
-            let botResp = BOT_MESSAGES[propertyName];
-            if (botResp) {
-                let botMsg = botResp[language];
-                if (botMsg) {
-                    return botMsg;
-                } else {
-                    return 'Error :: Bot Message is not available for Property ' + propertyName + ' on Language ' + language;
-                }
+    //     } else {
+    //         let botResp = BOT_MESSAGES[propertyName];
+    //         if (botResp) {
+    //             let botMsg = botResp[language];
+    //             if (botMsg) {
+    //                 return botMsg;
+    //             } else {
+    //                 return 'Error :: Bot Message is not available for Property ' + propertyName + ' on Language ' + language;
+    //             }
 
-            } else {
-                return 'Error :: Bot Message configuration is not available for Property ' + propertyName;
-            }
+    //         } else {
+    //             return 'Error :: Bot Message configuration is not available for Property ' + propertyName;
+    //         }
 
-            return BOT_MESSAGES[propertyName][language];
-        }
-    }
+    //         return BOT_MESSAGES[propertyName][language];
+    //     }
+    // }
 
-    BOT_MESSAGES_TEAMS = {
-        "INCIDENTSTATUSBYLIST": {
-            "ENGLISH": {
-                "title": "",
-                "subtitle": "",
-                "text": "Last 10 Incidents",
-                "buttons": [
-                    {
-                        "type": "imBack",
-                        "title": "Select",
-                        "value": "INC0010808"
-                    }
-                ]
-            }
-        }
-    }
+    // BOT_MESSAGES_TEAMS = {
+    //     "INCIDENTSTATUSBYLIST": {
+    //         "ENGLISH": {
+    //             "title": "",
+    //             "subtitle": "",
+    //             "text": "Last 10 Incidents",
+    //             "buttons": [
+    //                 {
+    //                     "type": "imBack",
+    //                     "title": "Select",
+    //                     "value": "INC0010808"
+    //                 }
+    //             ]
+    //         }
+    //     }
+    // }
 
     var greetingMessage = {
         'beginGreeting': (session, platform) => {
