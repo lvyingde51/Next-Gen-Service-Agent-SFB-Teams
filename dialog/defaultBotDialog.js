15ffa03db9f7828657181b82ae0d383fc2e9eb2b
Mubashshir Mohammed <39416@Hexaware.com>
2018-02-13 11:29:04 +0530

    MM : Handled flow of bot when service request Id is entered

--- dialog/defaultBotDialog.js
+++ dialog/defaultBotDialog.js
@@ -37,7 +37,6 @@
 
             if (textsess.match(incidentRegex) != null || textsess.match(serviceRequestRegex) != null) {
                 session.conversationData.capturedStr = session.message.text;
-                console.log('-- incident ID --', session.conversationData.capturedStr);
                 apiService.getStatusByNumber(session.conversationData.capturedStr, reqType, function (data) {
                     log.consoleDefault(JSON.stringify(data));
                     if (!data) {
@@ -50,21 +49,28 @@
                         let msg = 'Incident Number does not exist in our database. ' + data.error.message + ' Please try again';
                         session.endDialog(msg);
                     } else {
-                        session.conversationData.sys_id = data.result[0].sys_id;
-                        console.log('-- sys_id --', session.conversationData.sys_id);
-                        session.conversationData.incident_state = data.result[0].incident_state;
-                        session.conversationData.urgency = data.result[0].urgency;
-                        session.conversationData.category = data.result[0].category;
-                        session.conversationData.short_description = data.result[0].short_description;
-                        console.log('--status of incident-- ', session.conversationData.incident_state);
-                        let msg = 'Below are the details for the requested incident :- <br/>Incident Id : ' + session.conversationData.capturedStr + ' <br/>Short Description : ' + session.conversationData.short_description + ' <br/>Status: ' + jsonData.incidentStatus[session.conversationData.incident_state][lang];
-                        session.send(msg);
-                        // 1 - New | 2 - In Progress | 3 - On Hold | 6 - Resolved | 7 - Closed | 8 - Canceled
-                        if (session.conversationData.incident_state == 7 || session.conversationData.incident_state == 8) {
-                            builder.Prompts.choice(session, 'What do you want to do with the entered incident number?', ['Reopen']);
-                        } else {
-                            builder.Prompts.choice(session, 'What do you want to do with the entered incident number?', ['Add Comment', 'Close']);
+                        if(reqType === 'SERVICEREQUEST') {
+                            let msg = 'Below are the details for the requested incident :- <br/>Incident Id : ' + session.conversationData.capturedStr + ' <br/>Short Description : ' + session.conversationData.short_description + ' <br/>Status: ' + jsonData.incidentStatus[session.conversationData.incident_state][lang];
+                            session.endDialog(msg);
                         }
+
+                        if(reqType === 'SERVICEREQUEST') {
+                            session.conversationData.sys_id = data.result[0].sys_id;
+                            console.log('-- sys_id --', session.conversationData.sys_id);
+                            session.conversationData.incident_state = data.result[0].incident_state;
+                            session.conversationData.urgency = data.result[0].urgency;
+                            session.conversationData.category = data.result[0].category;
+                            session.conversationData.short_description = data.result[0].short_description;
+                            console.log('--status of incident-- ', session.conversationData.incident_state);
+                            let msg = 'These are the details of the requested Service Request:- <br/>Requested Item Number : ' + session.conversationData.SRNumber + ' <br/>Short Description : ' + data.result[0].short_description + ' <br/>Installation Status: ' + commonTemplate.incidentStatus[data.result[0].state][lang] + ' <br/>Approval: ' + data.result[0].approval.toUpperCase() + ' <br/>Stage: ' + data.result[0].stage.toUpperCase().split('_').join(' ') + ' <br/>Due Date: ' + data.result[0].due_date;
+                            session.send(msg);
+                            // 1 - New | 2 - In Progress | 3 - On Hold | 6 - Resolved | 7 - Closed | 8 - Canceled
+                            if (session.conversationData.incident_state == 7 || session.conversationData.incident_state == 8) {
+                                builder.Prompts.choice(session, 'What do you want to do with the entered incident number?', ['Reopen']);
+                            } else {
+                                builder.Prompts.choice(session, 'What do you want to do with the entered incident number?', ['Add Comment', 'Close']);
+                            }
+                        }                        
                     }
                 });
 
