e478184d795c8c8ea390bf82831569a9b7c2dc48
Mubashshir Mohammed <39416@Hexaware.com>
2018-02-16 12:11:55 +0530

    MM : Incident Number displayed in lower case at a scenario

--- dialog/incidentStatusDialog.js
+++ dialog/incidentStatusDialog.js
@@ -23,7 +23,7 @@
         session.conversationData.IncidentNumber = builder.EntityRecognizer.findEntity(args.entities, 'IncidentNumber') ? builder.EntityRecognizer.findEntity(args.entities, 'IncidentNumber').entity : '';
 
         if(session.conversationData.IncidentNumber === '' && session.message.text.toLowerCase().indexOf('inc') > -1 && incidentNumPattern.test(session.message.text.toLowerCase().substring(session.message.text.toLowerCase().indexOf('inc')))) {
-            session.conversationData.IncidentNumber = session.message.text.toLowerCase().substring(session.message.text.toLowerCase().indexOf('inc'));
+            session.conversationData.IncidentNumber = session.message.text.toLowerCase().substring(session.message.text.toLowerCase().indexOf('inc')).toUpperCase();
             log.consoleDefault(session.message.text.toLowerCase().indexOf('inc'));
         }
         log.consoleDefault(session.conversationData.IncidentNumber);
