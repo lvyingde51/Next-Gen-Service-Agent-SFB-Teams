cce0f90e1420b42b755addf1229febafc74c68cc
Mubashshir Mohammed <39416@Hexaware.com>
2018-02-20 13:35:23 +0530

    Adding logs to check validation on prompting the incident status

--- dialog/incidentStatusBotDialog.js
+++ dialog/incidentStatusBotDialog.js
@@ -50,7 +50,7 @@
         function (session, results) {
             let resp = results.response;
             let regex = commonTemplate.regexPattern['INCIDENTREGEX'];
-            console.log(resp.match(commonTemplate.regexPattern['INCIDENTREGEX']));
+            console.log(commonTemplate.regexPattern['INCIDENTREGEX'].test(resp));
             console.log(regex);
             console.log(resp);
             if (!resp.match(regex)) {
