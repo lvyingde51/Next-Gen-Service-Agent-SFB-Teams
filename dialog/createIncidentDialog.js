a1bd4a9d3ea0daee0a80bc61eba13e1dc94233de
Arun Prasaath <39402.hexaware@gmail.com>
2018-02-21 15:52:21 +0530

    AP : Cleaning and modularization

--- dialog/createIncidentDialog.js
+++ dialog/createIncidentDialog.js
@@ -1,17 +1,14 @@
 (function () {
     'use strict';
-    
     var builder = require('botbuilder');    
     var log = require('../utils/logs');
-
+    /* ### Matching Create Intent ### */
     exports.load = function(intentDialog) {
         intentDialog.matches('Create.Incident', createIncident)
     }
-    
+
+    /* ### After Matched Create Intent - Init the Bot Dialog ### */
     var createIncident = [(session,args) => {
-
-        log.consoleDefault('*** Create Incident ***'); // Console Start
-
         let entities = args.entities;
         session.conversationData.category = '';
         session.conversationData.shortDescription = '';
@@ -19,16 +16,13 @@
         session.conversationData.severity = builder.EntityRecognizer.findEntity(args.entities, 'urgency')?builder.EntityRecognizer.findEntity(args.entities, 'urgency').entity:'';
         session.conversationData.shortDescription = builder.EntityRecognizer.findEntity(args.entities, 'shortDescription')?builder.EntityRecognizer.findEntity(args.entities, 'shortDescription').entity:'';
         session.conversationData.category = builder.EntityRecognizer.findEntity(args.entities, 'category')?builder.EntityRecognizer.findEntity(args.entities, 'category').resolution.values[0]:'';
-        log.consoleDefault(JSON.stringify(args)); // Console Args
-        log.consoleDefault(JSON.stringify(entities)); // Console Entity
-        
+        log.consoleDefault('*** Create Incident ***');
+        log.consoleDefault('*** Create Incident - Arguments ***',JSON.stringify(args));
+        log.consoleDefault('*** Create Incident - Argument Entities ***',JSON.stringify(entities));        
         return session.beginDialog('createIncident', function(err) {
             if(err) {
-                session.send(new builder.Message().text('Error Occurred with incidentStatus: ' + err.message));
+                session.send(new builder.Message().text('Error Occurred with Create Incident: ' + err.message));
             }
-        });
-        
-        
+        });       
     }];    
-
 }());
