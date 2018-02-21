87824529fd4b81176720860a6ab22a5a89b076dc
Arun Prasaath <39402.hexaware@gmail.com>
2018-02-21 14:58:20 +0530

    AP : Cleaning and modularization

--- dialog/closeIncidentDialog.js
+++ dialog/closeIncidentDialog.js
@@ -1,30 +1,25 @@
 (function () {
     'use strict';
-    
     var builder = require('botbuilder');    
     var log = require('../utils/logs');
 
+    /* ### Matching Close Intent ### */
     exports.load = function(intentDialog) {
         intentDialog.matches('Close.Incident', closeIncident)
     }
-    
+
+    /* ### After Matched Close Intent - Init the Bot Dialog ### */
     var closeIncident = [(session,args) => {
-
-        log.consoleDefault('*** Close Incident ***'); // Console Start
-
         let entities = args.entities;
-        
+        /* ### Getting Incident Number from Entity Recognizer ### */
         session.conversationData.IncidentNumber = builder.EntityRecognizer.findEntity(args.entities, 'IncidentNumber')?builder.EntityRecognizer.findEntity(args.entities, 'IncidentNumber').entity:'';
-        log.consoleDefault(JSON.stringify(args)); // Console Args
-        log.consoleDefault(JSON.stringify(entities)); // Console Entity
-        
+        log.consoleDefault('*** Close Incident ***');
+        log.consoleDefault('*** Close Incident - Arguments ***',JSON.stringify(args));
+        log.consoleDefault('*** Close Incident - Argument Entities ***',JSON.stringify(entities));
         return session.beginDialog('closeIncident', function(err) {
             if(err) {
                 session.send(new builder.Message().text('Error Occurred with closeIncident: ' + err.message));
             }
         });
-        
-        
-    }];    
-
+    }];
 }());
